651efc1613077e550dcf06ac20287e4e7c08f050
coreyolden <coreyolden@gmail.com>
2018-11-13 22:03:04 -0500

    Update quadsolver.c

--- QS/quadsolver.c
+++ QS/quadsolver.c
@@ -48,7 +48,7 @@ int verifyinput(double a, double b, double c, double d){
 
     int valid = 1;
     if(!a){
-        printf("That number is not quadratic because a =0\n");
+        printf("That number is not quadratic because a = 0\n");
         valid = 0;
     }
     if(!normalnumber(a))
@@ -58,7 +58,7 @@ int verifyinput(double a, double b, double c, double d){
     if (!normalnumber(c))
         valid = 0;
     if(d<0){
-    printf("This is not quadratic because b²-4ac<0\n");
+    printf("This is not quadratic because b²-4ac < 0\n");
     valid = 0;
     }
     return valid;
@@ -68,7 +68,7 @@ void solveQuadratic(double a, double b, double c, double d){
 
     if (d == 0){
         double root = (-b + sqrt(d)) / (2 * a);
-        printf("One root was found was found (-b + √b²-4ac)/2a = %lf", root);
+        printf("One root was found was found (-b + √b²-4ac)/2a = %lf\n", root);
     }
     if (d > 0){
         double positiveRoot = (-b + sqrt(d)) / (2 * a);
