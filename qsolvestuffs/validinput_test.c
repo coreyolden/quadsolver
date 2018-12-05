07e283c2e8f9dcc64def8ea8a185bfc35e6da1cf
harleymisner <harley.e.misner@wmich.edu>
2018-12-05 12:28:56 -0500

    Update validinput_test.c

--- qsolvestuffs/validinput_test.c
+++ qsolvestuffs/validinput_test.c
@@ -4,6 +4,19 @@
 int main(void){
   coefficients coef;
   int ret;
-  
+  float a = 3.2;
   ret = validDouble(a);
+  assert_eq("input validation", ret, 1);
+  
+  a = 8;
+  ret = validDouble(a);
+  assert_eq("input validation", ret, 1);
+  
+  a = 0.0/0.0;
+  ret = validDouble(a);
+  assert_eq("input validation", ret, 0);
+  
+  a = 1.0/0.0;
+  ret = validDouble(a);
+  assert_eq("input validation", ret, 0);
 }
