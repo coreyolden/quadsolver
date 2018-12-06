e18f05bdb0e7944ed4cceee6399348a8e6bd5bb4
harleymisner <harley.e.misner@wmich.edu>
2018-12-06 14:28:20 -0500

    Update example.c

--- Spikes/IEEE/example.c
+++ Spikes/IEEE/example.c
@@ -1,1 +1,10 @@
 //Example of different IEEE values
+
+int main(void){
+  float infintity = 1.0/0.0;          //inf
+  float neginfintity = -1.0/0.0;      //-inf
+  float notNum = 0.0/0.0;             //NAN
+  float norm = 1.234567*10^3;         //Normalized
+  float denorm = 1.65*10^(-5);        //Denormalized
+}
+
