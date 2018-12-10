e0ebbaac31963baf5c94373280feea1d94bc3b18
coreyolden <coreyolden@gmail.com>
2018-12-10 09:26:20 -0500

    Delete Asserts.h

--- QuadSolver/Asserts.h
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#define assert_eq(str,a,b) {\
-  if( a != b ) { \
-    fprintf(stderr, "%s  LINE %d: %s, %d !== %d\n", \
-              __FILE__ , __LINE__ , str ,  a , b ); \
-  } \
-}
